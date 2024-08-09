a87a4ff3767cd81bf3088acaba4ba1f056c10ee6
Your Name <you@example.com>
2024-08-09 09:15:28 -0300

    imagem do projeto inserida

---
+++ README.md
@@ -0,0 +1,39 @@
+# CADASTRO LOGIN E HTML FUNCAO.
+
+>* `` Insere uma caixa de texto com funcoes de salvar para nomes adicionados na parte de colocar a mensagem, com opcoes de editar e excluir os dados adicionado na caixa de mensagem e tambem montando a estrutura do projeto``
+
+#  🛠 OQUE O JAVA APRESENTA 🛠
+
+>* ``Ele faz o cadastro de usuários e aprova o registro feito.``
+
+#  ⚒ FUNCIONALIDADES DO JAVA ⚒
+
+ >* `` A funcao acessar, verifica se os campo de nome foram preenchidos no formulário de login, validando a informacao.``
+
+>* ``A função salvarUser adiciona um nome na lista de usuários atualizando a tabela na página de cadastro e dando opcao de arrumar o campo de mensagem.``
+
+>* ``A função editar(i) atualiza o campo de entrada com o nome selecionado para edição.``
+
+>* ``A função excluir(i) remove o nome selecionado da lista e atualiza a tabela.``
+
+>* `` com partes que o array pega as posicao da function criar tabela, evitando possiveis lups infinitos. `` 
+
+
+# 🔧TECNOLOGIAS🔧
+
+>* JavaScript
+>* html
+>* function
+>* vscode
+>* alert
+>* array
+>* value
+>* else
+>* let
+>* if
+
+
+FRANCISCO
+
+
+![](img/tela-login.png)
--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 function acessar() {
    let loginEmail = document.getElementById('loginEmail').value;
    let loginSenha = document.getElementById('loginSenha').value;
-   
+
    if (!loginEmail || !loginSenha) {
       alert('favor preencher todos os campos');
 
@@ -9,40 +9,51 @@ function acessar() {
       // else{ alert('campos preenchisdos com sucesso'); }
       window.location.href = 'cadastro.html';
    }
+}
 
-   var dadosLista = [];
-   function salvarUser() {
-      let nomeUser = document.getElementById('nomeUser').value;
-   
-      if (nomeUser) {
-         dadosLista.push(nomeUser);
-         //console.log(dadosLista);
-         crialista();
-         document.getElementById("nomeUser").value = "";
-      } else {
-         alert("favor informe um nome para cadastro");
-      }
-   
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+//------------------------------------------------------------------------------------------------------------------------
+var dadosLista = [];
+function salvarUser() {
+   let nomeUser = document.getElementById('nomeUser').value;
+
+   if (nomeUser) {
+      dadosLista.push(nomeUser);
+      //console.log(dadosLista);
+      crialista();
+      document.getElementById("nomeUser").value = "";
+   } else {
+      alert("favor informe um nome para cadastro");
    }
-   function crialista() {
-      let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th> Ações </th></tr>";
-      for (let i = 0; i <= (dadosLista.length - 1); i++) {
-         tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type ='button' onclick = 'editar(parentNode.parentNode.rowIndex)'>editar</button ><button type ='button' onclick = 'excluir(parentNode.parentNode.rowIndex)'>excluir</button ></td ></tr ></td ></tr > ";
-         document.getElementById('tabela').innerHTML = tabela;
-   
-      }
+
+}
+
+// FUNCAO PARA CRIAR LISTA DE USUARIOS
+
+// PEGA NOME DE AREY E COLOCA PARA NOME DE USUARIOS
+
+// DEFINICAO DO TAMANHO AREY
+// PEGA A POSICAO QUE ESTA O AREY 
+function crialista() {
+   let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th> Ações </th></tr>";
+   for (let i = 0; i <= (dadosLista.length - 1); i++) {
+      tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type ='button' onclick = 'editar(parentNode.parentNode.rowIndex)'>editar</button ><button type ='button' onclick = 'excluir(parentNode.parentNode.rowIndex)'>excluir</button ></td ></tr ></td ></tr > ";
+      document.getElementById('tabela').innerHTML = tabela;
+
    }
-   function editar(i) {
-      document.getElementById("nomeUser").value = dadosLista[(i - 1)];
-      dadosLista.splice(dadosLista[(i - 1)], 1);
-   }
-   
-   // FUNCAO PARA EXCLUIR NOME DE LISTA
-   
-   function excluir (i){
-      dadosLista.splice((i - 1), 1);
-   
-   document.getElementById("tabela").deleteRow(i);
-   
-   }
-   
+}
+
+// FUNCAO PARA EDITAR NOME DE LISTA
+function editar(i) {
+   document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+   dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+
+// FUNCAO PARA EXCLUIR NOME DE LISTA
+
+function excluir (i){
+   dadosLista.splice((i - 1), 1);
+
+document.getElementById("tabela").deleteRow(i);
+
+}
