ddaa23921c4547906ed0d9f4fd4f219cbd2a8d61
Your Name <you@example.com>
2024-08-09 07:46:37 -0300

    coloca validacao de email

--- controller.js
+++ controller.js
@@ -1,30 +1,4 @@
-function acessar(){
-    let loginEmail = document.getElementById('loginEmail').value;
-    let loginSenha = document.getElementById('loginSenha').value;
-  
-       if(!loginEmail || !loginSenha){
-          alert('favor preencher todos os campos');
-  
-       }else{
-        // else{ alert('campos preenchisdos com sucesso'); }
-        window.location.href = 'cadastro.html';
-       }
- }
-  
- // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
- //------------------------------------------------------------------------------------------------------------------------
- var dadosLista = [];
- function salvarUser() {
-    let nomeUser = document.getElementById('nomeUser').value;
-    
-    if (nomeUser){
-         dadosLista.push(nomeUser);
-         console.log(dadosLista);
-    }else{
-        alert("favor informe um nome para cadastro");
-    }
-
- }
- 
-
+function acessar() {
+   let loginEmail = document.getElementById('loginEmail').value;
+   let loginSenha = document.getElementById('loginSenha').value;
 
