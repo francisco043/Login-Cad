6353d570ed6d930871ca10fc73b651cd24ba366d
Your Name <you@example.com>
2024-08-09 07:50:09 -0300

    Implementa função de cadastro de usuário

--- controller.js
+++ controller.js
@@ -1,4 +1,27 @@
 function acessar() {
    let loginEmail = document.getElementById('loginEmail').value;
    let loginSenha = document.getElementById('loginSenha').value;
+   
+   if (!loginEmail || !loginSenha) {
+      alert('favor preencher todos os campos');
 
+   } else {
+      // else{ alert('campos preenchisdos com sucesso'); }
+      window.location.href = 'cadastro.html';
+   }
+
+   var dadosLista = [];
+   function salvarUser() {
+      let nomeUser = document.getElementById('nomeUser').value;
+   
+      if (nomeUser) {
+         dadosLista.push(nomeUser);
+         //console.log(dadosLista);
+         crialista();
+         document.getElementById("nomeUser").value = "";
+      } else {
+         alert("favor informe um nome para cadastro");
+      }
+   
+   }
+   
